dc33c2aacadf0292fe1936c85d6a9de51910bd1a
dominikapasternak <dominikapasternak25@gmail.com>
2019-12-18 23:18:31 +0100

    1 commit

---
+++ Proxyy/src/test/java/ProxyTest.java
@@ -0,0 +1,12 @@
+public class ProxyTest {
+    public static void main(String[] args){
+        CommandExecutor executor = new CommandExecutorProxy("Someone", "pwd");
+        try {
+            executor.runCommand("ls -ltr");
+            executor.runCommand(" rm -rf abc.pdf");
+        } catch (Exception e) {
+            System.out.println("Exception Message::"+e.getMessage());
+        }
+
+    }
+}
